ade50915034fccbd7a9b3c3688d3c05bf3b46db8
Ritchie Turner <ritchie@conciert.io>
2023-09-24 07:52:50 -0300

    attr-machine no longer takes cluster data

--- targets/live/hooks.clj
+++ targets/live/hooks.clj
@@ -2,9 +2,10 @@
   (:require [concierto.core :as core]))
 
 (defn attr-cluster
-  "Create dynamic attribute map 'clusters' and will add whatever is
-   returned under the key cluster per cluster
-   e.g. {:east {:db {} :api [] :west {:db {} :api []}"
+  " Create dynamic attribute map 'clusters' and will add whatever is returned
+under the cluster key per cluster e.g.
+{:east {:db {...} :api [...] :cluster-name :east}
+ :west {:db {...} :api [...] :cluster-name :west} "
   [cluster]
   (let [db (flatten (core/select-machines :role :db :cluster cluster))
         api (flatten (core/select-machines :role :api :cluster cluster))]
@@ -12,11 +13,14 @@
      :api (map :ip api)
      :cluster-name cluster}))
 
+(def recno (atom 0))
+
+; silly example showing addition of a recno field
 (defn attr-machine
-  "Merges return value into current machine values. "
-  [_machine _cluster-data]
-  {:added "dynamically"})
+  " Merges return value into current machine values. "
+  [_machine]
+  {:recno (str (swap! recno inc))})
 
 
 (defn on-deploy [args]
-  (println "Deployed " args))
+  (println " Deployed " args))
